375ebfd2bf465b7d033c5b7a894d16310b62a3b5
Aditya Patel <aditya.patel@hotwax.co>
2025-06-07 18:37:57 +0530

    feat: add firebase auth with login and registration

---
+++ src/views/UserListPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Users</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <p>User list will appear here.</p>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue'
+</script>
