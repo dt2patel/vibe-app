4c70b8338238a31e8cf8a905dd52e3d908637849
Aditya Patel <heyadityapatel@gmail.com>
2025-06-08 13:34:01 +0530

    enable standalone mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,26 @@ import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Vibe App',
+        short_name: 'Vibe',
+        start_url: '/',
+        display: 'standalone',
+        scope: '/',
+        background_color: '#ffffff',
+        theme_color: '#3880ff',
+        icons: [ /* your icons here */ ]
+      }
+    })
   ],
   resolve: {
     alias: {
