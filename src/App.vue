9af35198bf968c13abfa3f9b4f3fa55240189e0a
Aditya Patel <aditya.patel@hotwax.co>
2025-06-13 08:04:14 +0530

    Add Firebase push notifications and settings

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <ion-app>
     <ion-router-outlet />
+    <ion-toast :is-open="showToast" :message="toastMessage" @didDismiss="showToast = false" />
   </ion-app>
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { ref, onMounted } from 'vue'
+import { IonApp, IonRouterOutlet, IonToast } from '@ionic/vue'
+import { registerForPushNotifications, listenToForegroundMessages } from '@/hooks/useNotifications'
+
+const toastMessage = ref('')
+const showToast = ref(false)
+
+onMounted(() => {
+  registerForPushNotifications()
+  listenToForegroundMessages((payload) => {
+    const title = payload.notification?.title || ''
+    const body = payload.notification?.body || ''
+    toastMessage.value = title ? `${title}: ${body}` : body
+    showToast.value = true
+  })
+})
 </script>
---
+++ src/views/TabsPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet />
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="users" href="/users">
+          <ion-label>Users</ion-label>
+        </ion-tab-button>
+        <ion-tab-button tab="settings" href="/settings">
+          <ion-label>Settings</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonLabel } from '@ionic/vue'
+</script>
